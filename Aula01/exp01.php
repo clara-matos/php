4912643951167864917783a965250fc6ca67ab81
Clara-matos <145491101+clara-matos@users.noreply.github.com>
2023-10-18 13:59:24 -0300

    Add files via upload

---
+++ Aula01/exp01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>exp01</title>
+</head>
+<body>
+    
+ <h1> teste auteração</h1>
+
+<?php 
+echo"aequivo em php"
+
+?>
+
+</body>
+</html>
