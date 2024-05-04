5f2d3829d222fce463830ac590e6e721b79b0e98
Clara-matos <145491101+clara-matos@users.noreply.github.com>
2024-05-04 17:27:19 -0300

    Add files via upload

---
+++ AULA-30-04/aula.php
@@ -0,0 +1,58 @@
+<?php
+$var = array (2,5,7,9,11);
+echo $var[0];
+echo "<br>";
+echo $var[1];
+echo "<br>";
+echo $var[2];
+echo "<br>";
+echo $var[3];
+echo "<br>";
+echo $var[4];
+echo "<br>";
+echo "<br>";
+
+$var1 = array ("Antonio","João","Zeca");
+echo $var1[0];
+echo "<br>";
+echo $var1[1];
+echo "<br>";
+echo $var1[2]; 
+echo "<br>"; 
+echo "<br>";           
+
+$var2 = array ("Windows", 10 , 2024 ,"V1.2021");
+echo $var[0];
+echo "<br>";
+echo $var2[1];
+echo "<br>";
+echo $var2[2]; 
+echo "<br>";
+echo $var2[3]; 
+echo "<br>";
+echo "<br>";
+
+$var3 = array ("Italo"=>30, "Hubert"=>20, "Luna"=>18);
+echo "<br>";
+echo " A idade de Italo é " .$var3['Italo'];
+echo "<br>";
+echo " A idade de Hubert é " .$var3['Hubert'];
+echo "<br>";
+echo " A idade de Luna é " .$var3['Luna'];
+echo "<br>";
+echo "<br>";
+
+$num = array (2 => 7, 3 => 8, 9 => 5);
+echo "<br>";
+echo $num[2];
+echo "<br>";
+echo $num[3];
+echo "<br>";
+echo $num[9];
+echo "<br>";
+echo "<br>";
+
+
+
+
+?>
