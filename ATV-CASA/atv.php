98ec5b97608d4e28e72c6c9a537de6de913c3f36
Clara-matos <145491101+clara-matos@users.noreply.github.com>
2024-05-11 13:30:44 -0300

    Add files via upload

---
+++ ATV-CASA/atv.php
@@ -0,0 +1,48 @@
+<?php
+
+$aluno["nome"] = "Ítalo Breno";
+$aluno["endereco"] = "Rua 2";
+$aluno["cidade"] = "guanambi";
+$aluno["estado"] = "bahia";
+$aluno["idade"] = "17";
+$aluno["cpf"] = "111.222.333-4";
+echo " aluno e" .$aluno ["nome"];
+echo "endereco" . $aluno ["endereco"];
+echo "sua cidade e". $aluno []
+
+$aluno1["nome"] = "Nicolas";
+$aluno1["endereco"] = "rua sei la";
+$aluno1["cidade"] = "tanque novo";
+$aluno1["estado"] = "bahia";
+$aluno1["idade"] = "18";
+$aluno1["cpf"] = "000.000.000-0";
+
+$aluno2["nome"] = "Lucas";
+$aluno2["endereco"] = "Rua lucas";
+$aluno2["cidade"] = "montes claros";
+$aluno2["estado"] = "minas gerais";
+$aluno2["idade"] = "22";
+$aluno2["cpf"] = "999.999.999-9";
+
+$professor["nome"] = "eber";
+$professor["endereco"] = "Rua dignissima";
+$professor["cidade"] = "eberlandia";
+$professor["estado"] = "bahia";
+$professor["idade"] = "30";
+$professor["materia"] = "banco de dados";
+
+$professor1["nome"] = "fabio";
+$professor1["endereco"] = "Rua";
+$professor1["cidade"] = "laboratorio";
+$professor1["estado"] = "bahia";
+$professor1["idade"] = "99";
+$professor1["materia"] = "psw";
+
+$professor1["nome"] = "silvia";
+$professor1["endereco"] = "caverna";
+$professor1["cidade"] = "pre historia";
+$professor1["estado"] = "pangeia";
+$professor1["idade"] = "∞";
+$professor1["materia"] = "portugues";
+
+?>
