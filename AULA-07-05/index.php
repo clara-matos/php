7542ceb44c430f00dc7b0272969771eaa20dac89
Clara-matos <145491101+clara-matos@users.noreply.github.com>
2024-05-11 13:31:03 -0300

    Add files via upload

---
+++ AULA-07-05/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h2> array: definição implincita</h2>
+    <pre> 
+         <?php
+           $var [0] = "joao";
+           $var [1] = "maria";
+           $var [2] = "bibi";
+           $var [3] = "vitoria";
+           $var [4] = "lucas";
+           $var [5] = "vinicio";
+           $var [6] = "isabela";
+           $var [7] = "clara";
+        echo "[for]: ";
+        for ($a=0; $a<count($var); $a++) {
+            echo "$var[$a]";
+        }
+       ?> 
+    </pre>
+    <h2> array </h2>
+
+</body>
+</html>
