ec468837956e0190e6af3fd780a02b42ab9f2641
Clara-matos <145491101+clara-matos@users.noreply.github.com>
2024-05-14 14:48:54 -0300

    Add files via upload

--- AULA-07-05/index.php
+++ AULA-07-05/index.php
@@ -10,21 +10,48 @@
     <h2> array: definição implincita</h2>
     <pre> 
          <?php
-           $var [0] = "joao";
-           $var [1] = "maria";
-           $var [2] = "bibi";
-           $var [3] = "vitoria";
-           $var [4] = "lucas";
-           $var [5] = "vinicio";
-           $var [6] = "isabela";
-           $var [7] = "clara";
-        echo "[for]: ";
-        for ($a=0; $a<count($var); $a++) {
-            echo "$var[$a]";
+           $aluno = array ( "clara", "luna", "Ryan", "Italo", "livia", "pedro", "Isa");
+        for ($a=0; $a<7; $a++) {
+            echo " $a = $aluno[$a] <br>";
         }
        ?> 
     </pre>
-    <h2> array </h2>
+    <h2> array: Definição Explicita </h2>
+     <pre>
+       <?php
+       $var = array (1, 2, 3, 4);
+       for ($a=0; $a<count($var); $a++) {
+           echo "$var[$a]";
+       }
+     ?>
+     </pre>
+     <h2> exemplo com foreach</h2>
+     <pre>
+      <?php
+      $var = ["Hubert" => 20, "CLara" => 5, "Luna" =>6.1, "Ryan" =>4.1];
+      echo "exemplo com foreach <br>";
+      $var = ["huber" => 3, "ariel" => 5, "luiz " => 6, "ryan" => 10];
+
+      foreach ($var as $chave => $valor){
+          if($valor >=6 && $valor<=10){
+              echo "aluno aprovado";
+              echo "o Aluno: $chave <br>";
+              echo" obteve a nota : $valor <br>";
+          } else if ($valor<6){
+              echo "aluno reprovados";
+              echo "o Aluno: $chave <br>";
+              echo "obteve a nota : $valor <br>";
+          }
+          
+      }
+      ?>
+     </pre>
+    
+     
+   
+    
+
+     
 
 </body>
 </html>
